fd4475a2c54110f1d3cf2f55168ebcf9115a3bc9
Anastasia <derricksmac@Anyas-MacBook-Air.local>
2022-06-08 01:02:03 -0400

    commit tree 54acd8e1cdee7595f20c7df654f35f52b97b7c4a

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(arr, eventName){
+    const cards = []
+    for (let i = 0; i< arr.length; i++){
+        cards.push(`Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`)
+    }return cards
+} 
+
+function countDown(num){
+    let countDown = num
+    while (countDown >= 0){
+        console.log(countDown--)
+    }
+}
